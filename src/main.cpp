f9c100e93f249c6363f76bdccf8322244f1294f2
Tarnished <tarnished.code@gmail.com>
2012-02-23 02:32:13 -0500

    General structure commit

---
+++ src/main.cpp
@@ -0,0 +1,16 @@
+/*
+ * main.cpp
+ *
+ *  Created on: Feb 23, 2012
+ *      Author: Tarnished - Micah
+ */
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	cout << "Hi";
+	return 0;
+}
+
